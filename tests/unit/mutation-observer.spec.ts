c9c41df5dfbb5a6fe77bf3315eca63113530f40b
mojahige <13248811+mojahige@users.noreply.github.com>
2023-05-30 09:03:59 +0900

    Add isHTMLElement test

---
+++ tests/unit/mutation-observer.spec.ts
@@ -0,0 +1,26 @@
+// @vitest-environment jsdom
+
+import { expect, test } from "vitest";
+import { isHtmlElement } from "../../src/mutation-observer";
+
+test("isHtmlElement returns true if HTMLElement is passed.", () => {
+  expect(isHtmlElement(document.createElement("div"))).toBe(true);
+});
+
+test.each([
+  document.createTextNode("div"),
+  true,
+  false,
+  "<p>foo</p>",
+  1234,
+  Symbol("foo"),
+  {},
+  [],
+  undefined,
+  null,
+])(
+  "isHtmlElement returns false if anything other than an HTMLElement is passed.",
+  (value) => {
+    expect(isHtmlElement(value)).toBe(false);
+  },
+);
